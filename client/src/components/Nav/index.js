46969ef7f4c1e4775033edd91e2c3c29fbfa2cf1
John George <68780394+johngeorge88@users.noreply.github.com>
2021-01-29 19:50:47 -0500

    adding the closing button to the sidebar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
+
 
 function Nav() {
 
@@ -36,23 +37,55 @@ function Nav() {
         }
     }
 
+    const [isClosed, setClosed] = React.useState(false)
     return (
-        <div className="bg-black text-white">
-            <div className="font-sans text-lg flex justify-between p-2 font-bold">
+        <div className="bg-black ">
+            <div className="font-sans text-white text-lg flex justify-between p-2 font-bold">
                 <Link to="/">
                     CREATIVE SQUARE
                 </Link>
             </div>
 
             <div className="flex bg-gray-100">
-                <aside className="bg-white w-64 min-h-screen flex flex-col">
-                    <span className="text-black py-2">Your Creative Square</span>
-                </aside>
-                <div className="flex justify-end">
-                    <nav>
-                        {showNavigation()}
-                    </nav>
-                </div>
+                {!isClosed && (
+                    <aside className="bg-white w-80 min-h-screen flex flex-col">
+                        <h1 className="text-black py-2 font-bold px-8">Your Creative Square</h1>
+
+                        <div className="border-r flex-grow">
+                            <nav>
+                                <ul>
+                                    <li className="p-3">
+                                        <a href="">
+                                            Login
+                                    </a>
+                                    </li>
+                                    <li className="p-3">
+                                        <a href="">
+                                            Signup
+                                    </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                            {/* <nav>
+                            {showNavigation()}
+                        </nav> */}
+                        </div>
+                    </aside>
+                )}
+                {isClosed ? (
+                    <button id="nav-toggle" className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white"
+                        onClick={() => setClosed(false)}>
+                        <svg
+                            className="fill-current h-3 w-3"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                        </svg>
+                    </button>
+                ) : (
+                        <button onClick={() => setClosed(true)}>X</button>
+                    )}
             </div>
         </div>
     );
