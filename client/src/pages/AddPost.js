8144bf75cd6727171f04cbcc221b51d041e6c246
Napo <Michaeljnapolitano@gmail.com>
2021-01-31 21:40:28 -0500

    styled create post form

--- client/src/pages/AddPost.js
+++ client/src/pages/AddPost.js
@@ -53,63 +53,133 @@ const AddPost = () => {
     } catch (err) {
       console.error(err);
     }
-   
+    window.alert(formState.postMediaType + " submited");
   };
 
   return (
-    <div>
-      <form className="inline-grid" onSubmit={handleFormSubmit}>
-        <ul>
-          <li className="text-lg font-bold">Post Type</li>
-          <input
+    <form
+      className="shadow-md flex-auto max-w-sm p-10 pb-20 bg-blue-300"
+      onSubmit={handleFormSubmit}
+    >
+      <h2 className="text-2xl text-center">Create a Post!</h2>
+      <div className="w-full">
+        <div className="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase">
+          <span className="text-red-400 mr-1">*</span> Media Type
+        </div>
+        <div className="my-2 bg-white p-1 flex border border-gray-200 rounded">
+          {" "}
+          <select
+            className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
             name="postMediaType"
-            type="text"
             value={formState.postMediaType}
             onChange={handleChange}
-          ></input>
-          <li className="text-lg font-bold">Post Description</li>
+          >
+            <option>choose media type...</option>
+            <option>Image</option>
+            <option>Video</option>
+            <option>Audio</option>
+          </select>{" "}
+        </div>
+      </div>
+      <div className="w-full">
+        <div className="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase">
+          <span className="text-red-400 mr-1">*</span> Post Description
+        </div>
+        <div className="my-2 bg-white p-1 flex border border-gray-200 rounded">
+          {" "}
           <input
+            className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
             name="postDescription"
             type="text"
             value={formState.postDescription}
             onChange={handleChange}
-          ></input>
-          <li className="text-lg font-bold">Post Link</li>
+          />{" "}
+        </div>
+      </div>
+      <div className="w-full">
+        <div className="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase">
+          <span className="text-red-400 mr-1">*</span> External URL
+        </div>
+        <div className="my-2 bg-white p-1 flex border border-gray-200 rounded">
+          {" "}
           <input
+            className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
             name="postLink"
             type="text"
             value={formState.postLink}
             onChange={handleChange}
-          ></input>
-          <li className="text-lg font-bold">Post image or video</li>
+          />{" "}
+        </div>
+      </div>
+      <div className="w-full">
+        <div className="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase">
+          <span className="text-red-400 mr-1">*</span> Upload Image
+        </div>
+        <div className="my-2 bg-white p-1 flex border border-gray-200 rounded">
+          {" "}
           <input
+            className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
             name="postPrimaryMedia"
             type="text"
             value={formState.postPrimaryMedia}
             onChange={handleChange}
-          ></input>
-          <li className="text-lg font-bold">Post audio file</li>
+          />{" "}
+        </div>
+      </div>
+      {/* <div className="w-full">
+        <div className="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase">
+          <span className="text-red-400 mr-1">*</span> Upload Video
+        </div>
+        <div className="my-2 bg-white p-1 flex border border-gray-200 rounded">
+          {" "}
           <input
+            className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
+            name="postPrimaryMedia"
+            type="upload"
+            value={formState.postPrimaryMedia}
+            onChange={handleChange}
+          />{" "}
+        </div>
+      </div> */}
+      <div className="w-full">
+        <div className="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase">
+          <span className="text-red-400 mr-1">*</span> Upload Audio File
+        </div>
+        <div className="my-2 bg-white p-1 flex border border-gray-200 rounded">
+          {" "}
+          <input
+            className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
             name="postSecondaryMedia"
-            type="text"
+            type="upload"
             value={formState.postSecondaryMedia}
             onChange={handleChange}
-          ></input>
-          <li className="text-lg font-bold">Post Paywall?</li>
-
+          />{" "}
+        </div>
+      </div>
+      <div className="w-full">
+        <div className="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase">
+          <span className="text-red-400 mr-1">*</span> post paywall
+        </div>
+        <div className="my-2 bg-white p-1 flex border border-gray-200 rounded">
+          {" "}
           <input
             name="postPaywall"
             type="text"
             value={formState.postPaywall}
             onChange={handleChange}
-          ></input>
-
-          <li>
-            <button type="submit">Submit</button>
-          </li>
-        </ul>
-      </form>
-    </div>
+          />{" "}
+        </div>
+      </div>
+      <div className="mt-6 relative">
+        <button
+          className="shadow-md font-medium py-2 px-4 bg-green-500 text-gray-100
+                  cursor-pointer bg-teal-600 rounded text-lg tr-mt  absolute text-center w-full"
+          type="submit"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
   );
 };
 
