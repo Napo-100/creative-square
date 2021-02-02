e6b3bd1cf42be8065b7c010588c7dcfcd9afb1b0
John George <68780394+johngeorge88@users.noreply.github.com>
2021-02-01 20:12:04 -0500

    fixing the modal cancel button

--- client/src/pages/AddPost.js
+++ client/src/pages/AddPost.js
@@ -230,12 +230,12 @@ const AddPost = () => {
                     <button type="submit" className="mb-1 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
                       ADD POST
                         </button>
-                  </form>
-                  <Link to="/">
+                        <Link to="/">
                     <button type="cancel" className="mt-4 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
                       Cancel
                       </button>
                   </Link>
+                  </form>
                 </div>
               </div>
             </div>
--- client/src/pages/UpdateUser.js
+++ client/src/pages/UpdateUser.js
@@ -183,14 +183,15 @@ const EditUser = () => {
                     </div>
                     <li>
                       <button className="mb-1 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm" type="submit">Submit</button>
+                      <Link to="/">
+                        <button type="cancel" className="mt-4 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                          Cancel
+                      </button>
+                      </Link>
                     </li>
                   </ul>
                 </form>
-                <Link to="/">
-                  <button type="cancel" className="mt-4 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
-                    Cancel
-                      </button>
-                </Link>
+
               </div>
             </div>
           </div>
