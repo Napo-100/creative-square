67cdf3254e313dc5839c03abe374ae0cbe52dcf6
Napo <Michaeljnapolitano@gmail.com>
2021-02-03 09:41:55 -0500

    put conditional render for post type back in master feed

--- client/src/pages/MasterFeed.js
+++ client/src/pages/MasterFeed.js
@@ -19,20 +19,23 @@ const MasterFeed = () => {
       {/* {loading && <div>Loading...</div>} */}
       <div className="grid grid-cols-1 gap-1 md:grid-cols-2 md:gap-1 lg:grid-cols-3 lg:gap-2 xl:grid-cols-3 pt-2">
         {posts.map((post) => (
-          <div key={post._id} className="hover:opacity-80 bg-transparent rounded-lg border-4">
+          <div
+            key={post._id}
+            className="hover:opacity-80 bg-transparent rounded-lg border-4"
+          >
             <article className="hover:shadow-xl group">
               {/* <a href={post.postLink} target="_blank" rel="noreferrer"> */}
               {/* make link to single post page */}
               <Link to={`/post/${post._id}`}>
-
                 <span className="block h-64 shadow leading-snug flex flex-wrap content-center">
-                  <img
-                    src={post.postPrimaryMedia}
-                    className="object-cover h-full w-max rounded-lg"
-                    alt="post"
-                  />
+                  {post.postMediaType === "Audio " && (
+                    <img
+                      src={post.postPrimaryMedia}
+                      className="object-cover h-full w-max rounded-lg"
+                      alt="post"
+                    />
+                  )}
 
-               
                   {post.postMediaType === "Video" && (
                     <iframe
                       src={post.postPrimaryMedia}
@@ -40,7 +43,6 @@ const MasterFeed = () => {
                       alt="post"
                     />
                   )}
-
                 </span>
               </Link>
               {/* </a> */}
