d4943794bb23d5ffe6a1553e2abe483d94e589bb
Jrsparl <john@sparlingmusic.com>
2021-02-03 04:27:51 -0500

    all feeds functioning

--- client/src/pages/FollowFeed.js
+++ client/src/pages/FollowFeed.js
@@ -8,31 +8,38 @@ const FollowFeed = () => {
   const { loading, data } = useQuery(QUERY_ME);
 //   console.log("query following data: " + userData);
 
-const posts = data?.me.following || [];
-    console.log("these are posts ", posts);
+const following = data?.me.following || [];
+    console.log("these are posts ", following[1].username);
 
   return (
     <div className="container pt-3">
       {/* {loading && <div>Loading...</div>} */}
       <div className="grid grid-cols-1 gap-1 md:grid-cols-2 md:gap-1 lg:grid-cols-3 lg:gap-0 xl:grid-cols-3 p-2">
-        {posts.map((post) => (
-          <div key={post._id} className="hover:opacity-80 bg-transparent rounded-lg">
-            <article className="hover:shadow-xl group">
-              {/* <a href={post.postLink} target="_blank" rel="noreferrer"> */}
-              {/* make link to single post page */}
-              <Link to={`/post/${post._id}`}>
-                <span className="block h-64 shadow leading-snug flex flex-wrap content-center">
-                  <img
-                    src={post.postPrimaryMedia}
-                    className="object-cover h-full w-max"
-                    alt="post"
-                  />
-                </span>
-              </Link>
-              {/* </a> */}
-            </article>
-          </div>
-        ))}
+        {
+        following.map((follow) => 
+        
+        follow.posts.map((post)=>
+        <div key={post._id} className="hover:opacity-80 bg-transparent rounded-lg">
+        <article className="hover:shadow-xl group">
+          {/* <a href={post.postLink} target="_blank" rel="noreferrer"> */}
+          {/* make link to single post page */}
+          <Link to={`/post/${post._id}`}>
+            <span className="block h-64 shadow leading-snug flex flex-wrap content-center">
+              <img
+                src={post.postPrimaryMedia}
+                className="object-cover h-full w-max"
+                alt="post"
+              />
+            </span>
+          </Link>
+          {/* </a> */}
+        </article>
+      </div>)
+
+
+
+
+        )}
       </div>
     </div>
   );
