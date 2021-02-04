cde4bf1f86cd245b0e004a711f29688a9e92f03e
John George <68780394+johngeorge88@users.noreply.github.com>
2021-02-03 20:59:53 -0500

    fixing the profile bar

--- client/src/components/SideBar/index.js
+++ client/src/components/SideBar/index.js
@@ -31,20 +31,20 @@ function SideBar() {
               </li>
               </Link>
             ) : (
-              <div className="flex flex-row flex-wrap justify-center sm:flex sm:flex-col">
+              <div className="flex flex-row flex-grow justify-center sm:flex sm:flex-col">
                 {/* this link goes to finishprofile temporarily, but will be /edituser when complete */}
                 <Link to="/finishprofile">
-                  <li className="border-b-4 hover:bg-gray-300 p-2">EditUser</li>
+                  <li className="flex-grow border-b-4 hover:bg-gray-300 p-2 w-20 sm:w-full">EditUser</li>
                 </Link>
                 <Link to="/postform">
-                  <li className="border-b-4 hover:bg-gray-300 p-2">Add Post</li>
+                  <li className="flex-grow border-b-4 hover:bg-gray-300 p-2 w-20 sm:w-full ml-2 sm:ml-0">Add Post</li>
                 </Link>
-                <li className="border-b-4 hover:bg-gray-300 p-2">
+                <button className="text-left border-b-4 hover:bg-gray-300 p-2 w-20 sm:w-full ml-2 sm:ml-0">
                   {/* this is not using the Link component to logout or user and then refresh the application to the start */}
                   <a href="/" onClick={() => Auth.logout()}>
                     Logout
-            </a>
-                </li>
+                  </a>
+                </button>
               </div>
             )}
 
@@ -66,11 +66,12 @@ function SideBar() {
   return (
     <div>
       <div className="bg-gray-100 sticky top-0 ">
-      <div className=" p-2 ml-2 sm:px-20 sm:pb-10 w-10 sm:hidden">
-            <img src={Logo} className="pt-5" alt="Creative Square" />
+      <div className=" p-2 ml-2 flex flex-wrap content-center sm:hidden">
+            <img src={Logo} className="pt-5 w-10" alt="Creative Square" />
+            <h3>CREATIVE SQUARE</h3>
           </div>
         <aside className="flex flex-row sm:flex-col sm:justify-center bg-white sm:w-80 sm:min-h-screen pt-5 sm:pt-0">
-          <div className=" p-2 ml-2 sm:px-20 sm:pb-10 hidden  sm:flex">
+          <div className=" p-2 ml-2 sm:px-20 sm:pb-10 hidden sm:flex">
             <img src={Logo} className="pt-5" alt="Creative Square" />
           </div>
           <div className="border-r sm:flex-grow ">
