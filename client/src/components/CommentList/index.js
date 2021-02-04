029d14b38853485ba10541a35d9d9f70b256b0a0
Napo <Michaeljnapolitano@gmail.com>
2021-02-03 21:52:06 -0500

    fixed error of class not being className

--- client/src/components/CommentList/index.js
+++ client/src/components/CommentList/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 {
-  /* <div class="bg-white rounded-lg p-3 flex flex-col justify-center items-center md:items-start shadow-lg mb-4">
-<div class="flex flex-row justify-center mr-2">
+  /* <div className="bg-white rounded-lg p-3 flex flex-col justify-center items-center md:items-start shadow-lg mb-4">
+<div className="flex flex-row justify-center mr-2">
 
-<h3 class="text-purple-600 font-semibold text-lg text-center md:text-left ">@Shanel</h3>
+<h3 className="text-purple-600 font-semibold text-lg text-center md:text-left ">@Shanel</h3>
 
 </div>
 
-<p style={{width:"90%"}} class="text-gray-600 text-lg text-center md:text-left ">Hi good morning will it be the entire house. </p>
+<p style={{width:"90%"}} className="text-gray-600 text-lg text-center md:text-left ">Hi good morning will it be the entire house. </p>
 
 
 </div> */
@@ -27,17 +27,17 @@ const CommentList = ({ comments }) => {
         comments.map((comment) => (
           <div
             key={comment._id}
-            class="bg-white rounded-lg p-3 flex flex-col justify-center items-center md:items-start shadow-lg mb-4"
+            className="bg-white rounded-lg p-3 flex flex-col justify-center items-center md:items-start shadow-lg mb-4"
           >
-            <div class="flex flex-row justify-center mr-2">
-              <h3 class="text-purple-600 font-semibold text-lg text-center md:text-left ">
+            <div className="flex flex-row justify-center mr-2">
+              <h3 className="text-purple-600 font-semibold text-lg text-center md:text-left ">
                 {comment.username}
               </h3>
             </div>
 
             <p
               style={{ width: "90%" }}
-              class="text-gray-600 text-lg text-center md:text-left "
+              className="text-gray-600 text-lg text-center md:text-left "
             >
               {comment.commentText}{" "}
             </p>
--- client/src/pages/MasterFeed2.js
+++ client/src/pages/MasterFeed2.js
@@ -21,49 +21,49 @@ const MasterFeed2 = () => {
 
         <div className="max-w-lg border-4">
           <div
-            class="grid grid-cols-1 grid-rows-7 grid-flow-row overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            className="grid grid-cols-1 grid-rows-7 grid-flow-row overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
           >
-            <div class="col-span-3 row-span-4 p-1 m-1 justify-center">
+            <div className="col-span-3 row-span-4 p-1 m-1 justify-center">
               <a href={`/post/${post._id}`}>
                 <img
                   src={post.postPrimaryMedia}
                   alt="Placeholder"
-                  class="rounded-t-xl object-contain w-max max-h-96"
+                  className="rounded-t-xl object-contain w-max max-h-96"
                 />
               </a>
             </div>
             <ul
               style={{ overflow: "hidden" }}
-              class="flex flex-row pl-2 text-gray-600 overflow-x-scroll hide-scroll-bar">
+              className="flex flex-row pl-2 text-gray-600 overflow-x-scroll hide-scroll-bar">
 
               <ReactionPanel post={post} />
 
             </ul>
 
-            <div class="col-span-3 row-span-1">
-              <div class="flex align-bottom flex-col leading-none p-2 md:p-4">
-                <div class="flex flex-row justify-between items-center">
+            <div className="col-span-3 row-span-1">
+              <div className="flex align-bottom flex-col leading-none p-2 md:p-4">
+                <div className="flex flex-row justify-between items-center">
                   <a
-                    class="flex items-center no-underline hover:underline text-black"
+                    className="flex items-center no-underline hover:underline text-black"
                     href="#"
                   >
                     <img
                       alt="Placeholder"
                       style={{ height: "32px", width: "32px" }}
-                      class="block rounded-full"
+                      className="block rounded-full"
                       src="https://picsum.photos/640/400/?random"
                     />
-                    <span class="ml-2 text-sm"> {post.username} </span>
+                    <span className="ml-2 text-sm"> {post.username} </span>
                   </a>
                 </div>
               </div>
             </div>
 
-            <div class="col-span-3 row-span-1">
+            <div className="col-span-3 row-span-1">
               <header
-                class="flex items-center justify-between leading-tight p-2 md:p-4"
+                className="flex items-center justify-between leading-tight p-2 md:p-4"
               >
-                <p class="text-sm">
+                <p className="text-sm">
 
                   {post.postDescription}
 
@@ -72,8 +72,8 @@ const MasterFeed2 = () => {
               </header>
             </div>
 
-            <div class="col-span-3 row-span-1">
-              <p class="text-grey-darker text-xs p-2 md:p-4">{post.createdAt}</p>
+            <div className="col-span-3 row-span-1">
+              <p className="text-grey-darker text-xs p-2 md:p-4">{post.createdAt}</p>
             </div>
           </div>
         </div>
