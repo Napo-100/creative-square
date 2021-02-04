64534c904a031747a0a17bf04dfe6e0f4e40a010
Napo <Michaeljnapolitano@gmail.com>
2021-02-03 22:45:17 -0500

    added video to singlePost page

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -26,14 +26,31 @@ const SinglePost = () => {
         <div className="max-w-lg border-4">
           <div className="grid grid-cols-1 grid-rows-7 grid-flow-row overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
             <div className="col-span-3 row-span-4 p-1 m-1 justify-center">
-              <a href="#">
-                <img
-                  src={post.postPrimaryMedia}
-                  alt="Placeholder"
-                  className="rounded-t-xl object-contain w-max max-h-96"
-                />
-              </a>
+              {post.postMediaType === "Video" && (
+                <div className="flex justify-center">
+                  <a href="#">
+                    <iframe
+                      src={post.postPrimaryMedia}
+                      className="rounded h-96"
+                      style={{ width: "32rem" }}
+                      alt="post"
+                    />
+                  </a>
+                </div>
+              )}
+              {post.postMediaType === "Image" && (
+                <div className="flex justify-center">
+                  <a href="#">
+                    <img
+                      src={post.postPrimaryMedia}
+                      alt="Placeholder"
+                      className="rounded-t-xl object-contain w-max max-h-96"
+                    />
+                  </a>
+                </div>
+              )}
             </div>
+
             <ul
               style={{ overflow: "hidden" }}
               className="flex flex-row pl-2 text-gray-600 overflow-x-scroll hide-scroll-bar"
